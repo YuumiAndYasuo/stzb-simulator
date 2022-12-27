eaadc0baad00df7010bc48f255d67a2a92639327
YuumiAndYasuo <1522426323@qq.com>
2022-12-27 20:47:45 +0800

    init

---
+++ src/main/java/Character.java
@@ -0,0 +1,54 @@
+public class Character {
+
+    Long Id;
+    String Name;
+    Skill firstSkill;
+    Skill secondSkill;
+    Skill thirdSkill;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Skill getFirstSkill() {
+        return firstSkill;
+    }
+
+    public void setFirstSkill(Skill firstSkill) {
+        this.firstSkill = firstSkill;
+    }
+
+    public Skill getSecondSkill() {
+        return secondSkill;
+    }
+
+    public void setSecondSkill(Skill secondSkill) {
+        this.secondSkill = secondSkill;
+    }
+
+    public Skill getThirdSkill() {
+        return thirdSkill;
+    }
+
+    public void setThirdSkill(Skill thirdSkill) {
+        this.thirdSkill = thirdSkill;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("lsajdflasdkfj");
+    }
+}
---
+++ src/main/java/Skill.java
@@ -0,0 +1,30 @@
+//技能
+public class Skill {
+    Integer Id;
+    String Name;
+    String Des;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDes() {
+        return Des;
+    }
+
+    public void setDes(String des) {
+        Des = des;
+    }
+}
